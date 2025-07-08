7774a0aee768d3877d076f4015527a286d2910a1
anukul rai <anukul.rau@gmail.com>
2025-07-09 00:47:43 +0545

    909

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
+  const navigate =useNavigate()
+  const hanldeSignIn=(e)=>{
+    e.preventDefault()
+    navigate('/register')
+  }
   return (
     <div>
-      
+      <nav>
+        <button 
+        className='border px-4 py-2 rounded-full bg-yellow-600 border-none cursor-pointer hover:bg-blue-600 transition duration-400 text-white font-semiboldte'
+        onClick={hanldeSignIn}>Sign In</button>
+      </nav>
     </div>
   )
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
 function Dashboard() {
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-r from-black via-gray-900 to-black'>
-      <h1>Dashboard</h1>
+      <div className='bg-white p-5 rounded-xl'>
+              <Link to='/'><h1>Dashboard</h1></Link>
+      </div>
     </div>
   )
 }
